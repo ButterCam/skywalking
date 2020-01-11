c6dde76e59e9f1f77bd0c71f488ea54f70996be3
wz <wangzheng@bybutter.com>
2020-01-11 23:54:10 +0800

    Override expect data

--- test/plugin/scenarios/grpc-scenario/src/main/java/org/apache/skywalking/apm/testcase/grpc/provider/service/GreeterBlockingErrorServiceImpl.java
+++ test/plugin/scenarios/grpc-scenario/src/main/java/org/apache/skywalking/apm/testcase/grpc/provider/service/GreeterBlockingErrorServiceImpl.java
@@ -26,8 +26,6 @@ import org.apache.skywalking.apm.testcase.grpc.proto.HelloRequest;
 public class GreeterBlockingErrorServiceImpl extends GreeterBlockingErrorGrpc.GreeterBlockingErrorImplBase {
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        responseObserver.onNext(HelloReply.newBuilder().setMessage("Hi," + request.getName()).build());
         responseObserver.onError(new Exception());
-        responseObserver.onCompleted();
     }
 }
